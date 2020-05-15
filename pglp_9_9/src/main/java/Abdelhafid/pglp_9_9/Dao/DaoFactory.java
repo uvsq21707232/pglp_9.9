b9a3e3c72c14944267adea9e3c0fc692f0ae2064
abdelhafid belhabib <abdelhafid.belhabib@ens.uvsq.fr>
2020-05-15 16:18:24 +0200

    implementation de la fabrique Dao pour creer des formes DAO JDBC

---
+++ pglp_9_9/src/main/java/Abdelhafid/pglp_9_9/Dao/DaoFactory.java
@@ -0,0 +1,46 @@
+package Abdelhafid.pglp_9_9.Dao;
+
+import Abdelhafid.pglp_9_9.Formes.Carre;
+import Abdelhafid.pglp_9_9.Formes.Cercle;
+import Abdelhafid.pglp_9_9.Formes.Rectangle;
+import Abdelhafid.pglp_9_9.Formes.Triangle;
+
+public  class  DaoFactory {
+	
+	/**
+     * Crée un CarreDaoJdbc.
+     * @return  CareeDaoJdbc crée.
+     */
+	public  DaoJDBC<Carre> getCarreDaoJdbc () {
+		return new CarreDaoJdbc();
+		
+		}
+	
+	/**
+     * Crée un CercleDaoJdbc.
+     * @return  CercleDaoJdbc crée.
+     */
+	public  DaoJDBC<Cercle> getCercleDaoJdbc () {
+		return new CercleDaoJdbc();
+		
+		}
+	
+	/**
+     * Crée un TriangleDaoJdbc.
+     * @return  TriangleDaoJdbc crée.
+     */
+	public  DaoJDBC<Triangle> getTriangleDaoJdbc () {
+		return new TriangleDaoJdbc();
+		
+		}
+	
+	/**
+     * Crée un RectangleDaoJdbc.
+     * @return  RectangleDaoJdbc crée.
+     */
+	public  DaoJDBC<Rectangle> getRectangleDaoJdbc () {
+		return new RectangleDaoJdbc();
+		
+		}
+	
+}
